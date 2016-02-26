f4cb856eb2d12aa948995e682abdc99ee281a131
gallinagreta <gallinagreta@Lab06_06.itseinaudi.local>
2016-02-26 12:12:14 +0100

    CIAO:)

--- Slot machine/src/SlotMachine.java
+++ Slot machine/src/SlotMachine.java
@@ -1,5 +1,7 @@
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.SWT;
 
 public class SlotMachine {
 
@@ -31,9 +33,28 @@ public class SlotMachine {
 	
 	protected void createContents() {
 		shell = new Shell();
-		shell.setSize(450, 300);
+		shell.setSize(504, 449);
 		shell.setText("SWT Application");
+		
+		Button btnReset = new Button(shell, SWT.NONE);
+		btnReset.setBounds(10, 327, 75, 63);
+		btnReset.setText("RESET");
+		
+		Button btnPlayTable = new Button(shell, SWT.NONE);
+		btnPlayTable.setText("PLAY TABLE");
+		btnPlayTable.setBounds(89, 327, 75, 63);
+		
+		Button btnBetOne = new Button(shell, SWT.NONE);
+		btnBetOne.setText("BET ONE");
+		btnBetOne.setBounds(170, 327, 75, 63);
+		
+		Button btnBetMax = new Button(shell, SWT.NONE);
+		btnBetMax.setText("BET MAX");
+		btnBetMax.setBounds(251, 327, 75, 63);
+		
+		Button btnSpin = new Button(shell, SWT.NONE);
+		btnSpin.setText("SPIN");
+		btnSpin.setBounds(382, 327, 75, 63);
 
 	}
-
 }
