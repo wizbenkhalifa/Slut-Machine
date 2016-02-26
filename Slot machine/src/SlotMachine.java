98f8d44f492fb0b2814cf1dcf7aace2e4f26276b
franceschinnicolo <franceschinnicolo@Lab06_08.itseinaudi.local>
2016-02-26 12:56:17 +0100

    tg4yg5ryh

--- Slot machine/src/SlotMachine.java
+++ Slot machine/src/SlotMachine.java
@@ -2,10 +2,16 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 
 public class SlotMachine {
 
 	protected Shell shell;
+	private Text text;
+	private Text text_1;
+	private Text text_2;
 
 	
 	public static void main(String[] args) {
@@ -35,7 +41,6 @@ public class SlotMachine {
 		shell = new Shell();
 		shell.setSize(504, 449);
 		shell.setText("SWT Application");
-		
 		Button btnReset = new Button(shell, SWT.NONE);
 		btnReset.setBounds(10, 327, 75, 63);
 		btnReset.setText("RESET");
@@ -45,16 +50,55 @@ public class SlotMachine {
 		btnPlayTable.setBounds(89, 327, 75, 63);
 		
 		Button btnBetOne = new Button(shell, SWT.NONE);
+		btnBetOne.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				int puntata;
+				try {
+					puntata=Integer.parseInt(text_1.getText());
+					puntata++;
+					text_1.setText(Integer.toString(puntata));
+				} catch (NumberFormatException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
 		btnBetOne.setText("BET ONE");
 		btnBetOne.setBounds(170, 327, 75, 63);
 		
 		Button btnBetMax = new Button(shell, SWT.NONE);
+		btnBetMax.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				int allin;
+				try {
+					allin=Integer.parseInt(text.getText());
+					text_1.setText(Integer.toString(allin));
+				} catch (NumberFormatException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+			}
+		});
 		btnBetMax.setText("BET MAX");
 		btnBetMax.setBounds(251, 327, 75, 63);
 		
 		Button btnSpin = new Button(shell, SWT.NONE);
 		btnSpin.setText("SPIN");
 		btnSpin.setBounds(382, 327, 75, 63);
+		
+		text = new Text(shell, SWT.BORDER);
+		text.setText("3456");
+		text.setBounds(10, 246, 216, 53);
+		
+		text_1 = new Text(shell, SWT.BORDER);
+		text_1.setText("0");
+		text_1.setBounds(232, 246, 94, 53);
+		
+		text_2 = new Text(shell, SWT.BORDER);
+		text_2.setBounds(333, 246, 145, 53);
 
 	}
 }
