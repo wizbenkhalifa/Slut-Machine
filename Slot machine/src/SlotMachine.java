82f59ab45fceaadfe3c2d414e58a733bde5c2524
benkhalifayoussef <benkhalifayoussef@Lab06_07.itseinaudi.local>
2016-02-26 12:04:50 +0100

    Inizio progetto

---
+++ Slot machine/src/SlotMachine.java
@@ -0,0 +1,39 @@
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class SlotMachine {
+
+	protected Shell shell;
+
+	
+	public static void main(String[] args) {
+		try {
+			SlotMachine window = new SlotMachine();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(450, 300);
+		shell.setText("SWT Application");
+
+	}
+
+}
